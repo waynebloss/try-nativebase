4f98ff550b03c7ef55b34427f3b7c9bc8f887ec8
Wayne Bloss <waynebloss@gmail.com>
2018-07-24 11:07:33 -0400

    Load native-base fonts.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {
+  AppLoading,
+  // Asset,
+  Font,
+  // Icon,
+} from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.loadResourcesAsync}
+          onError={this.handleLoadingError}
+          onFinish={this.handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    }
   }
+
+  loadResourcesAsync = async () => {
+    return Promise.all([
+      // Asset.loadAsync([
+      //   require('./src/assets/images/abc.png'),
+      //   require('./src/assets/images/xyz.png'),
+      // ]),
+      Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      }),
+    ]);
+  };
+
+  handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
 
 const styles = StyleSheet.create({
