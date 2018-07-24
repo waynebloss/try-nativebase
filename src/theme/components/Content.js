03f85a342b13a2cc4a39e8cee5437129f530aa85
Wayne Bloss <waynebloss@gmail.com>
2018-07-24 10:54:35 -0400

    Run lint. Replace all double-quotes with single-quotes. Replace tabs with spaces. Comment out unused vars.

--- src/theme/components/Content.js
+++ src/theme/components/Content.js
@@ -1,12 +1,12 @@
-import variable from "./../variables/platform";
+import variable from './../variables/platform';
 
 export default (variables = variable) => {
   const contentTheme = {
     flex: 1,
-    backgroundColor: "transparent",
-    "NativeBase.Segment": {
+    backgroundColor: 'transparent',
+    'NativeBase.Segment': {
       borderWidth: 0,
-      backgroundColor: "transparent"
+      backgroundColor: 'transparent'
     }
   };
 
